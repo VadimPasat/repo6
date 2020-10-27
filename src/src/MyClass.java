8b75ee8c96638c56e5750a5e279cba8676b76767
Vadim Pasat <vadim.pasat@endava.com>
2020-10-27 23:24:54 +0200

    Commit 1

---
+++ src/src/MyClass.java
@@ -0,0 +1,7 @@
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello all from earth");
+        System.out.println("Task 8");
+    }
+}
